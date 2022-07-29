93fc4e0bc38b4dd87773559e52b6fad1f5f8edfc
moemawla <moemawla@hotmail.com>
2022-07-29 20:07:41 +1000

    Adds logic to create the users table

--- user_upload.php
+++ user_upload.php
@@ -18,15 +18,47 @@ if (!array_key_exists('u', $argumentList) ||
     !array_key_exists('p', $argumentList) ||
     !array_key_exists('h', $argumentList)
 ) {
-    die('Please provide the credentials for connecting to the database');
+    logMessage('Please provide the credentials for connecting to the database');
+    exit;
 }
 
 // create and check database connection
-$conn = new mysqli($argumentList['h'], $argumentList['u'], $argumentList['p'], DATABASE_NAME);
+$dbConnection = new mysqli($argumentList['h'], $argumentList['u'], $argumentList['p'], DATABASE_NAME);
 
-if ($conn->connect_error) {
-  die('Connection failed: ' . $conn->connect_error);
+if ($dbConnection->connect_error) {
+    logMessage('Connection failed: ' . $dbConnection->connect_error);
+    exit;
+}
+
+if (array_key_exists('create_table', $argumentList)) {
+    createTable($dbConnection);
+    exit;
 }
 
 // close database connection
-$conn->close();
+$dbConnection->close();
+
+/**
+ * declare helper functions
+ */
+
+function logMessage(string $message) {
+    fwrite(STDOUT, $message);
+}
+
+function createTable(mysqli $connection) {
+    $query = <<<SQL
+    CREATE TABLE IF NOT EXISTS users (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    surname VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE
+    )
+SQL;
+
+    if (true === $connection->query($query)) {
+        logMessage('Users table created successfully');
+        return;
+    }
+    logMessage('Failed creating the users table');
+}
