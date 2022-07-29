29b0c6d0c4ca7dbda69a2cb569db83fd34500b1a
moemawla <moemawla@hotmail.com>
2022-07-29 19:14:54 +1000

    Validates database credentials are passed and connects to the database

--- user_upload.php
+++ user_upload.php
@@ -11,3 +11,17 @@ $longOptions = [
 
 $argumentList = getopt($shortOptions, $longOptions);
 
+// make sure the database credentials are passed
+if (!array_key_exists('u', $argumentList) ||
+    !array_key_exists('p', $argumentList) ||
+    !array_key_exists('h', $argumentList)
+) {
+    die('Please provide the credentials for connecting to the database');
+}
+
+// create and check database connection
+$conn = new mysqli($argumentList['h'], $argumentList['u'], $argumentList['p']);
+
+if ($conn->connect_error) {
+  die('Connection failed: ' . $conn->connect_error);
+}
